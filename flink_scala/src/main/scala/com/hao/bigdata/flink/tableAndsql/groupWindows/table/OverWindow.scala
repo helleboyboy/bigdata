e9ee03c9de02092f9f625fd8e7ca7265b5d1850a
mr thinker <1786076351@qq.com>
2022-03-13 18:56:50 +0800

    table风格的over windows实现

--- flink_scala/src/main/scala/com/hao/bigdata/flink/tableAndsql/groupWindows/table/OverWindow.scala
+++ flink_scala/src/main/scala/com/hao/bigdata/flink/tableAndsql/groupWindows/table/OverWindow.scala
@@ -1,33 +1,36 @@
 package com.hao.bigdata.flink.tableAndsql.groupWindows.table
+
 import com.hao.bigdata.flink.source.SensorSource
+import org.apache.calcite.rex.RexWindowBounds.preceding
 import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.table.api._
 import org.apache.flink.table.api.bridge.scala._
 import org.apache.flink.types.Row
+
 /**
  *
  * @Author: Java页大数据
- * @Date: 2022-03-13:18:09
+ * @Date: 2022-03-13:18:34
  * @Describe:
- *    完成over windows的实现
- *    多去了解hive的开窗函数！
  */
 object OverWindow {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-    val sourceDataStream = env.addSource(new SensorSource)
+    val sourceStream = env.addSource(new SensorSource)
       .assignAscendingTimestamps(_.timestamp)
     val settings = EnvironmentSettings.newInstance()
       .inStreamingMode()
       .useBlinkPlanner()
       .build()
     val tEnv = StreamTableEnvironment.create(env, settings)
-    tEnv.createTemporaryView("sensor_overWindows", sourceDataStream, 'id, 'timestamp.rowtime as 'ts, 'temperature)
-    val sqlString = "SELECT id, COUNT(id) OVER(PARTITION BY id ORDER BY ts ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) FROM sensor_overWindows"
-    tEnv.sqlQuery(sqlString)
+    val table = tEnv.fromDataStream(sourceStream, 'id, 'timestamp.rowtime as 'ts, 'temperature)
+    table
+      .window(Over partitionBy 'id orderBy 'ts preceding UNBOUNDED_RANGE as 'w)
+      //不需要再groupby，select的时候，需要加上over 窗口参数！
+      .select('id, 'id.count over 'w)
       .toRetractStream[Row]
       .print()
     env.execute()
